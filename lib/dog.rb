564512f59946692c48df4b1fa42db9eee8fd0940
Jessica Pereira <jcpereira070@gmail.com>
2020-06-23 17:39:58 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+  def initialize(name,breed="Mutt")
+    @name = name
+    @breed = breed
+
+
+  end
+end
+#
+#   def name=(name)
+#     @name = name
+#   end
+#
+#   def name
+#     @name
+#   end
+# end
+#
+# name = Dog.new("Beyonce")
