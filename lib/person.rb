bfe09e9a93fc1e02c5d005bed712f6d4cf7e88d6
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-27 21:18:01 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def initialize (persons_name= Beyonce)
+    @name = persons_name
+  end
+
+  def name=(persons_name= Beyonce)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
+  
+end
